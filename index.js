7646d40faad1169bd958d9cf227e256cfc66bcf7
Stefan Holmberg <stefan.holmberg@systementor.se>
2025-02-25 11:20:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express()
+const port =  3000;
+
+app.get('/',(req,res)=>{
+    res.send('<html><body><h1>Hej</h1><p>hej2</p></body></html>')
+})
+
+const players = [
+    {id:1,jersey:21,name:"Foppa"},
+    {id:2,jersey:2,name:"Anders Eldebrink"},
+    {id:3,jersey:13,name:"Sudden"}
+]
+ 
+
+app.get('/movies',(req,res)=>{
+    res.json(players)
+})
+
+
+app.get('/contact',(req,res)=>{
+    res.send('Dont contact us')
+})
+app.listen( port ,()=>{
+    console.log('server is running at port number 3000')
+});
